6895e6c02fcc52df1a62910a671aa05c36f022a3
SweetCherry <artwis1998@gmail.com>
2023-12-09 15:34:23 +0100

    Add adventure of code day 6 part 1

---
+++ AdventOfCode-6/main.dart
@@ -0,0 +1,52 @@
+import '../utils.dart';
+
+//--- Day 6: Wait For It ---
+
+class Race {
+  final int duration;
+  final int distanceRecord;
+
+  const Race({required this.duration, required this.distanceRecord});
+
+  int findBeatingHoldingStrategiesCount(){
+    final List<int> strategies = [];
+    int testCaseLeft = duration ~/ 2;
+    int testCaseRight = testCaseLeft+1;
+    while(testCaseLeft > 0 || testCaseRight < duration) {
+      if((testCaseLeft > 0) && (calculateDistanceAtHoldingTime(time: testCaseLeft) > distanceRecord)) {
+        strategies.add(testCaseLeft);
+        testCaseLeft--;
+      } else {
+        testCaseLeft = 0;
+      }
+      if((testCaseRight < duration) && (calculateDistanceAtHoldingTime(time: testCaseRight) > distanceRecord)) {
+        strategies.add(testCaseRight);
+        testCaseRight++;
+      } else {
+        testCaseRight = duration;
+      }
+    }
+    return strategies.length;
+  }
+
+  int calculateDistanceAtHoldingTime({required int time}) {
+    assert(duration > time);
+    return time * (duration-time);
+  }
+}
+
+Future<void> main() async {
+  final inputText = await getInput(fileName: 'input.txt');
+  final [_, durationsText] = inputText[0].split(':');
+  final [_, distancesText] = inputText[1].split(':');
+  final durations = onlyNumbersRegExp.allMatches(durationsText).map((e) => int.parse(e.group(0)!)).toList();
+  final distances = onlyNumbersRegExp.allMatches(distancesText).map((e) => int.parse(e.group(0)!)).toList();
+  int multiplication = 1;
+  for (int i = 0; i < durations.length; i++) {
+    final strategiesNumber = Race(duration: durations[i], distanceRecord: distances[i]).findBeatingHoldingStrategiesCount();
+    print(strategiesNumber);
+    multiplication *= strategiesNumber;
+  }
+  print(multiplication);
+
+}
