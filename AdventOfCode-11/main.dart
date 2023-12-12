015f12d970a60d49dd52994e10a7b32230bfbde0
SweetCherry <artwis1998@gmail.com>
2023-12-12 10:48:14 +0100

    Add Advent of Code day 11 part 2

--- AdventOfCode-11/main.dart
+++ AdventOfCode-11/main.dart
@@ -3,9 +3,11 @@ import '../utils.dart';
 class Universe {
   final List<Galaxy> galaxies;
   final List<List<String>> map;
-  final bool isExpanded;
+  bool isCosmicSpacesInitialised;
+  final List<int> xCosmicSpaces = [];
+  final List<int> yCosmicSpaces = [];
 
-  const Universe._(this.galaxies, this.map, this.isExpanded);
+  Universe._(this.galaxies, this.map, this.isCosmicSpacesInitialised);
 
   factory Universe.fromInput(List<String> input) {
     List<List<String>> map = [];
@@ -15,29 +17,20 @@ class Universe {
     return Universe._([], map, false);
   }
 
-  void applyCosmicExpansion() {
-    assert(!isExpanded, 'Universe is already expanded');
-    final List<List<String>> mapCopy = [];
-    for (List<String> row in map) {
-      mapCopy.add(List<String>.from(row));
-    }
-
-    final int startWidth = mapCopy[0].length;
-    for (int y = 0; y < mapCopy.length; y++) {
-      final rowCopy = List<String>.from(mapCopy[y]);
-      if (rowCopy.every((tile) => tile == '.')) {
-        final indexToInsert = y + map.length - mapCopy.length;
-        map.insert(indexToInsert, List.generate(startWidth, (_) => '.'));
+  void findCosmicSpaces() {
+    assert(!isCosmicSpacesInitialised, 'Universe is already expanded');
+    isCosmicSpacesInitialised = true;
+    for (int y = 0; y < map.length; y++) {
+      final row = map[y];
+      if (row.every((tile) => tile == '.')) {
+        yCosmicSpaces.add(y);
       }
     }
-    for (int x = 0; x < startWidth; x++) {
-      final columnCopy = mapCopy.map((row) => row[x]).toList();
-      if (columnCopy.every((tile) => tile == '.')) {
-        for (int y = 0; y < map.length; y++) {
-          final int currentWidth = map[y].length;
-          final int indexToInsert = x + currentWidth - startWidth;
-          map[y].insert(indexToInsert, '.');
-        }
+    final width = map[0].length;
+    for (int x = 0; x < width; x++) {
+      final column = map.map((row) => row[x]).toList();
+      if (column.every((tile) => tile == '.')) {
+        xCosmicSpaces.add(x);
       }
     }
   }
@@ -52,14 +45,21 @@ class Universe {
     }
   }
 
-  int sumOfShortestDistances() {
+  int sumOfShortestDistances({int cosmicSpaceDistance = 1}) {
+    assert(isCosmicSpacesInitialised, 'Cosmic spaces are not initialised');
+
     int sum = 0;
     for (int i = 0; i < galaxies.length - 1; i++) {
       for (int j = i + 1; j < galaxies.length; j++) {
         final Galaxy galaxy = galaxies[i];
         final Galaxy otherGalaxy = galaxies[j];
         if (galaxy != otherGalaxy) {
-          int distance = galaxy.calculateShortestDistanceTo(galaxy: otherGalaxy);
+          int distance = galaxy.calculateShortestDistanceTo(
+            other: otherGalaxy,
+            xCosmicSpaces: xCosmicSpaces,
+            yCosmicSpaces: yCosmicSpaces,
+            cosmicSpaceDistance: cosmicSpaceDistance,
+          );
           sum += distance;
         }
       }
@@ -79,18 +79,62 @@ class Galaxy {
 
   Galaxy(this.position);
 
-  int calculateShortestDistanceTo({required Galaxy galaxy}) {
-    final int xDistance = (position.x - galaxy.position.x).abs();
-    final int yDistance = (position.y - galaxy.position.y).abs();
+  int calculateShortestDistanceTo({
+    required Galaxy other,
+    required List<int> xCosmicSpaces,
+    required List<int> yCosmicSpaces,
+    required int cosmicSpaceDistance,
+  }) {
+    final int xDistance = (position.x - other.position.x).abs() +
+        countVerticalCosmicSpacesBetween(other: other, yCosmicSpaces: yCosmicSpaces) * (cosmicSpaceDistance - 1);
+    final int yDistance = (position.y - other.position.y).abs() +
+        countHorizontalCosmicSpacesBetween(other: other, xCosmicSpaces: xCosmicSpaces) * (cosmicSpaceDistance - 1);
     return xDistance + yDistance;
+  }
+
+  int countVerticalCosmicSpacesBetween({
+    required Galaxy other,
+    required List<int> yCosmicSpaces,
+  }) {
+    int count = 0;
+    for (int yCosmicSpace in yCosmicSpaces) {
+      if (position.y > other.position.y) {
+        if (yCosmicSpace > other.position.y && yCosmicSpace < position.y) {
+          count++;
+        }
+      } else {
+        if (yCosmicSpace > position.y && yCosmicSpace < other.position.y) {
+          count++;
+        }
+      }
+    }
+    return count;
+  }
+
+  int countHorizontalCosmicSpacesBetween({
+    required Galaxy other,
+    required List<int> xCosmicSpaces,
+  }) {
+    int count = 0;
+    for (int xCosmicSpace in xCosmicSpaces) {
+      if (position.x > other.position.x) {
+        if (xCosmicSpace > other.position.x && xCosmicSpace < position.x) {
+          count++;
+        }
+      } else {
+        if (xCosmicSpace > position.x && xCosmicSpace < other.position.x) {
+          count++;
+        }
+      }
+    }
+    return count;
   }
 }
 
 Future<void> main() async {
   List<String> input = await getInput(fileName: 'input.txt');
   Universe universe = Universe.fromInput(input)
-    ..applyCosmicExpansion()
+    ..findCosmicSpaces()
     ..findGalaxies();
-  //universe.printMap();
-  print(universe.sumOfShortestDistances());
+  print(universe.sumOfShortestDistances(cosmicSpaceDistance: 1000000));
 }
