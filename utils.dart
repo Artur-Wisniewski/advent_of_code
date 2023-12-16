229c4491691baeab929418243812a8cdc89ff801
SweetCherry <artwis1998@gmail.com>
2023-12-16 14:10:23 +0100

    Extract direction to utils

--- utils.dart
+++ utils.dart
@@ -34,3 +34,60 @@ class Position {
     return '($x, $y)';
   }
 }
+
+enum Directions {
+  north._(0, -1),
+  south._(0, 1),
+  east._(1, 0),
+  west._(-1, 0);
+
+  const Directions._(this.offsetX, this.offsetY);
+
+  final int offsetX;
+  final int offsetY;
+
+  Directions get opposite {
+    return switch (this) {
+      Directions.north => Directions.south,
+      Directions.south => Directions.north,
+      Directions.east => Directions.west,
+      Directions.west => Directions.east,
+    };
+  }
+
+  bool isVertical() {
+    return switch (this) {
+      Directions.north => true,
+      Directions.south => true,
+      Directions.east => false,
+      Directions.west => false,
+    };
+  }
+
+  bool isHorizontal() {
+    return switch (this) {
+      Directions.north => false,
+      Directions.south => false,
+      Directions.east => true,
+      Directions.west => true,
+    };
+  }
+
+  Directions get leftMirror {
+    return switch (this) {
+      Directions.north => Directions.west,
+      Directions.south => Directions.east,
+      Directions.east => Directions.south,
+      Directions.west => Directions.north,
+    };
+  }
+
+  Directions get rightMirror {
+    return switch (this) {
+      Directions.north => Directions.east,
+      Directions.south => Directions.west,
+      Directions.east => Directions.north,
+      Directions.west => Directions.south,
+    };
+  }
+}
